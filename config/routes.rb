344f29933337d65566635925af986c35aab4da17
Baiqing Wang <baiqing.wang66@gmail.com>
2011-01-29 13:23:17 -0600

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
   #get "pages/home"
 
   #get "pages/contact"
@@ -9,6 +11,7 @@ SampleApp::Application.routes.draw do
 	match '/contact', :to => 'pages#contact'
 	match '/about', :to => 'pages#about'
 	match '/help', :to => 'pages#help'
+	match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
