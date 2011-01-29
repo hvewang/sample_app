ccc98aa0dda2848d3bc34c02be96131fdf6bfd81
Baiqing Wang <baiqing.wang66@gmail.com>
2011-01-29 14:08:42 -0600

    Added logo, langimg, and rspec case

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
+	
+	def logo
+		image_tag("greatwall.png", :alt => "New Hope International Education", :class => "round")
+	end
+	
+	def lanImg
+		image_tag("zh.png", :alt => "New Hope International Education", :class => "round") 
+	end
 end
