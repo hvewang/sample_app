f7218cfc9effac055939ae3212de12af32b15989
Baiqing Wang <baiqing.wang66@gmail.com>
2011-03-09 00:14:06 -0600

    Changed force_utf function

--- app/models/translation.rb
+++ app/models/translation.rb
@@ -3,6 +3,5 @@ class Translation < ActiveRecord::Base
 	
 	def force_utf
 		#attributes.each {|key, value| value.force_encoding('utf-8') if value.encoding != 'utf-8' if value.kind_of?(String) }
-		attributes.each {|key, value| value.force_encoding('utf-8') if value.kind_of?(String) }
 	end
 end
