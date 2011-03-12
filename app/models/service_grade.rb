e16a2438e06e73ebb4c9016bdb40b19529d3bd32
Baiqing Wang <baiqing.wang66@gmail.com>
2011-03-12 15:06:27 -0600

    comments out force_utf

--- app/models/service_grade.rb
+++ app/models/service_grade.rb
@@ -2,6 +2,6 @@ class ServiceGrade < ActiveRecord::Base
 	validates :grade, :presence => true, :uniqueness => true
 	
 	def force_utf
-		attributes.each {|key, value| value.force_encoding('utf-8') if value.encoding != 'utf-8' if value.kind_of?(String) }
+		#attributes.each {|key, value| value.force_encoding('utf-8') if value.encoding != 'utf-8' if value.kind_of?(String) }
 	end
 end
