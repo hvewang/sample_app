55e8c0a64387dc9c8dc0f4bee391d0101ea08481
Baiqing Wang <baiqing.wang66@gmail.com>
2011-05-22 22:08:52 -0500

    news updates desc

--- app/models/news_update.rb
+++ app/models/news_update.rb
@@ -1,6 +1,5 @@
 class NewsUpdate < ActiveRecord::Base
-	default_scope :order => "news_type, created_at desc"
-	
+	default_scope :order => "created_at desc, news_type"
 	def force_utf
 		if Rails.env.development? 
 			attributes.each {|key, value| value.force_encoding('utf-8') if value.encoding != 'utf-8' if value.kind_of?(String) }
