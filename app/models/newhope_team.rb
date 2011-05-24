407e38d99bd10bba29a3ef068b36a4b1d56d70bb
Baiqing Wang <baiqing.wang66@gmail.com>
2011-05-23 22:00:31 -0500

    new hope pic

--- app/models/newhope_team.rb
+++ app/models/newhope_team.rb
@@ -1,11 +1,4 @@
-class NewhopeTeam < ActiveRecord::Base
-	has_attached_file :nhteampic, 
-		:styles => { :medium => "320x240>", :thumb => "160x120>", :large => "640x480>" },
-		:storage => :s3,
-		:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-		:path => ":attachment/:id/:style.:extension",
-		:bucket => 'NewHope-Prod'
-		
+class NewhopeTeam < ActiveRecord::Base	
 	def force_utf
 		if Rails.env.development? 
 			attributes.each {|key, value| value.force_encoding('utf-8') if value.encoding != 'utf-8' if value.kind_of?(String) }
