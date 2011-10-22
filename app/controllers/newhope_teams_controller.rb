e7b1ca11c903266131943e67175307ab74886bf0
Baiqing Wang <baiqing.wang66@gmail.com>
2011-10-22 17:32:11 -0500

    nh team list

--- app/controllers/newhope_teams_controller.rb
+++ app/controllers/newhope_teams_controller.rb
@@ -6,7 +6,7 @@ class NewhopeTeamsController < ApplicationController
   # GET /newhope_teams
   # GET /newhope_teams.xml
   def index
-    @newhope_teams = NewhopeTeam.all.paginate(:page => params[:page], :per_page => 10)
+    @newhope_teams = NewhopeTeam.find(:all, :order=> 'updated_at desc').paginate(:page => params[:page], :per_page => 10)
 
     respond_to do |format|
       format.html # index.html.erb
